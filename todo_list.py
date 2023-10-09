879da7db7ed8d59f416a94c66e5d3a013f5ff8a4
CodenameTobi <91787660+CodenameTobi@users.noreply.github.com>
2023-10-09 15:38:38 +0200

    Deleting all tasks method added

--- todo_list.py
+++ todo_list.py
@@ -1,18 +1,11 @@
-task_dict_model = {
-    "name" : "Task",
-    "description" : "This is an example task",
-    "due_date" : "yyyy/mm/dd , hh/mm",
-    "status" : "incomplete"
-}
 
 DATABANK = r"taskData/todo.txt"
 
-
-def update_databank(file, updated_tasks):
-    print("Updating tasks in databank")
-
+def clear_databank(databank):
+    open(databank, 'w').close()        
 
 def add_task(databank):
+    clear_databank(databank)
     name, description, due_date = get_task_from_user()
     with open(databank, 'a') as db:
         db.write("\n" + "-" * 100 + "\n")
@@ -20,7 +13,7 @@ def add_task(databank):
         db.write("Description:\n\t\t" + description + "\n")
         db.write("Due Date:\t" + due_date + "\n")
         db.write("Status:\t\tincomplete\n")
-        db.write("\n" + "-" * 100 + "\n")
+        db.write("-" * 100 + "\n")
 
 
 def get_task_from_user():
@@ -36,20 +29,30 @@ def display_tasks(databank):
         print(display_tasks) 
 
 def complete_task():
+    # TODO: maybe
     print("Task completed")
 
+
 def delete_task(task):
+    # TODO
     print("Delete Task")
+
+def delete_all(databank):
+    # all tasks in the databank are deleted
+    print("Deleting all tasks out of " + databank + "\n")
+    with open(databank, 'w') as db:
+        db.write("Empty")
 
 def menu():
     # this function handles the user interaction
     # this includes adding task, displaying tasks, deleting tasks and updating them
-    choice, min_value, max_value = (1000, 0, 3)
+    choice, min_value, max_value = (1000, 0, 4)
     menu_header = "-" * 8 + "Main menu" + "-" * 22
     menu_options = "\t[0] - Exit\n" \
                     "\t[1] - Add task\n" \
                     "\t[2] - Display tasks\n" \
-                    "\t[3] - Delete task\n"
+                    "\t[3] - Delete task\n" \
+                    "\t[4] - Delete all tasks\n"
     menu_end = "-" * 39
     print(menu_header + "\n" + menu_options + menu_end)
     while choice < min_value or choice > max_value:
@@ -78,6 +81,9 @@ def main():
         elif exit == 3:
             # Delete task
             print("Delete task")
+        elif exit == 4:
+            print("Delete all tasks")
+            delete_all(DATABANK)
         else:
             print("This has not been implemented yet!")
 
